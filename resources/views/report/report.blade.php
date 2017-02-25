e488abed362fb3c7b2f882ceee3f262e21fa3132
Sagar Chamling <sgr.raee@gmail.com>
2017-02-25 10:37:40 +0545

    Minor changes in report

--- resources/views/report/report.blade.php
+++ resources/views/report/report.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container">
-        <table class="table">
+        {{--<table class="table">
             <thead>
             <tr>
                 <th><abbr title="Position">ID</abbr></th>
@@ -26,6 +26,32 @@
                         <td>{{ $report->url }}</td>
                         <td>{{ $report->reason }}</td>
                         <td>{{ $report->user->name }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>--}}
+
+        <table class="table">
+            <thead>
+            <tr>
+                <th><abbr title="Position">No</abbr></th>
+                <th><abbr title="Website Url">Website Url</abbr></th>
+                <th><abbr title="Reported Times">Times</abbr></th>
+            </tr>
+            </thead>
+            <tfoot>
+            <tr>
+                <th><abbr title="Position">No</abbr></th>
+                <th><abbr title="Website Url">Website Url</abbr></th>
+                <th><abbr title="Reported Times">Times</abbr></th>
+            </tr>
+            </tfoot>
+            <tbody>
+                @foreach ($reports as $key => $report)
+                    <tr>
+                        <td>{{ $key+1 }}</td>
+                        <td>{{ $report->url }}</td>
+                        <td>{{ $report->times }}</td>
                     </tr>
                 @endforeach
             </tbody>
