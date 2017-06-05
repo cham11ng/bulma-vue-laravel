2d438c7af39a1a63ba577d54b97178648896f6a3
Sagar Chamling <sgr.raee@gmail.com>
2017-06-05 12:49:54 +0545

    Change Alert page

--- resources/views/alert/alert.blade.php
+++ resources/views/alert/alert.blade.php
@@ -1,22 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-	<div class="container">
-		<section class="section" id="notification-app">
-			<notification type="danger">
-				<span>Go to hell</span>
-			</notification>
-			<notification type="info">
-				<span>Go to Heaven</span>
-			</notification>
-			<notification type="success">
-				<span>Go no where</span>
-			</notification>
-		</section>
-	</div>
+	<section class="section" id="notification-app">
+		<notification type="danger">
+			<span>Go to hell</span>
+		</notification>
+		<notification type="info">
+			<span>Go to Heaven</span>
+		</notification>
+		<notification type="success">
+			<span>Go no where</span>
+		</notification>
+	</section>
 @endsection
 
 @section('scripts')
-	<script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
-	<script type="text/javascript" src="{{ asset('js/notification.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/notification.js') }}"></script>
 @endsection
