d614ee3810d0553d828a550a72a6075031356de5
Sagar Chamling <sgr.raee@gmail.com>
2017-06-08 00:35:32 +0545

    Vue js form I

--- resources/views/report/create.blade.php
+++ resources/views/report/create.blade.php
@@ -12,41 +12,73 @@
                 </div>
             @endif
 
-            <form role="form" method="POST" action="{{ url('reports') }}">
-                {{ csrf_field() }}
-                <label class="label">Website Url</label>
-                <p class="control {{ $errors->has('url') ? ' has-icon has-icon-right' : '' }}">
-                    <input type="text" class="input {{ $errors->has('url') ? ' is-danger' : '' }}" name="url" value="{{ old('url') }}" autofocus="autofocus">
-
-                    @if ($errors->has('url'))
-                        <span class="icon is-small">
-                                <i class="fa fa-warning"></i>
-                            </span>
-                        <span class="help is-danger">
-                                {{ $errors->first('url') }}
-                            </span>
-                    @endif
+            <form role="form" method="POST" action="{{ url('reports') }}" @submit.prevent="onSubmit" @keydown="errors.clear($event.target.name)">
+                <label class="label" for="url">Website Url</label>
+                <p class="control">
+                    <input v-model="url" id="url" type="text" class="input" name="url" autofocus="autofocus"/>
+                    <span class="help is-danger" v-if="errors.has('url')">@{{ errors.get('url') }}</span>
                 </p>
 
                 <label class="label">Your message</label>
-                <p class="control {{ $errors->has('reason') ? ' has-icon has-icon-right' : '' }}">
-                    <textarea class="textarea  {{ $errors->has('reason') ? ' is-danger' : '' }}" placeholder="Please elaborate ..." name="reason">{{ old('reason') }}</textarea>
-                    @if ($errors->has('reason'))
-                        <span class="icon is-small">
-                                <i class="fa fa-warning"></i>
-                            </span>
-                        <span class="help is-danger">
-                                {{ $errors->first('reason') }}
-                            </span>
-                    @endif
+                <p class="control">
+                    <textarea v-model="reason" class="textarea" placeholder="Please elaborate ..." name="reason"></textarea>
+                    <span class="help is-danger" v-if="errors.has('reason')">@{{ errors.get('reason') }}</span>
                 </p>
 
                 <div class="control is-grouped">
                     <p class="control">
-                        <button class="button is-primary" type="submit">Submit</button>
+                        <button class="button is-primary" type="submit" :disabled="errors.any()">Submit</button>
                     </p>
                 </div>
             </form>
         </div>
     </div>
+@endsection
+
+@section('scripts')
+    <script>
+        class Errors {
+            constructor() {
+                this.errors = {};
+            }
+
+            has(field) {
+                return this.errors.hasOwnProperty(field);
+            }
+
+            get(field) {
+                if (this.errors[field]) {
+                    return this.errors[field][0];
+                }
+            }
+
+            record(errors) {
+                this.errors = errors;
+            }
+
+            any() {
+                return Object.keys(this.errors).length > 0;
+            }
+
+            clear(field) {
+                delete this.errors[field];
+            }
+        }
+
+        new Vue({
+            el: '#app',
+            data: {
+                url: '',
+                reason: '',
+                errors: new Errors()
+            },
+            methods: {
+                onSubmit() {
+                    axios.post('/reports', this.$data)
+                        .then(response => alert('Success'))
+                        .catch(error => this.errors.record(error.response.data))
+                }
+            }
+        });
+    </script>
 @endsection
